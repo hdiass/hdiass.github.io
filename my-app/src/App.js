8d19caf3f5db98257327190154dcd8b856725992
Helder Dias <helder.dias@capitalise.com>
2019-09-10 13:11:31 +0100

    Upgrade styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap'
-
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+function Welcome(props) {
+  return <article>
+  <div class="article-title">
+    <a href="https://hdiass.github.io" title={props.title}>{props.title}</a>
+  </div>
+  {props.articleShrink}
+</article>
+}
 function App() {
   return (
     <div className="App">
@@ -14,10 +23,10 @@ function App() {
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossorigin="anonymous"
         />
+        <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono&display=swap" rel="stylesheet"></link>
       </header>
-      <body>
       <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Terminal diaries</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
@@ -37,10 +46,22 @@ function App() {
             </Form>
           </Navbar.Collapse>
         </Navbar>
-        <h1>
-        Helder Dias co.
-        </h1>
-      </body>
+        <div class="articles">
+        <Container>
+         <Row></Row>
+          <Row>
+            <Col>
+            <Welcome title="Some Sweet Article" articleShrink="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
+            {/* <article>
+              <div class="article-title">
+                <a href="https://hdiass.github.io" title="Some Sweet Article">Some Sweet Article</a>
+              </div>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </article> */}
+            </Col>
+          </Row>
+        </Container>
+        </div>
     </div>
   );
 }
