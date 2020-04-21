4afdd297ff938324fe97dada6feba9dcaf21be18
Helder Dias <helder.dias@capitalise.com>
2020-04-21 15:46:42 +0100

    Add react-router and multiple routes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import About from './About'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/about" component={About} />
+        </Switch>
+    </ BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
